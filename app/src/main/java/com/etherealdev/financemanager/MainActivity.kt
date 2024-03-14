e828101db59e553bb049c9a60a5500e637c8438b
Vishnu Sai Nath Karumrui <vishnusai.karumuri@gmail.com>
2024-03-15 00:07:22 +0530

    Project Created Start With : - Splash Screen - Login Activity - Firebase Backend

---
+++ app/src/main/java/com/etherealdev/financemanager/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.etherealdev.financemanager
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.etherealdev.financemanager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+
+}
