02035a73f84cd814ce88021d6fcc166de573a18c
Vishnu Sai Nath Karumrui <vishnusai.karumuri@gmail.com>
2024-03-17 19:40:02 +0530

    Project Created Start With : - Splash Screen - Login Activity - Firebase Backend

--- app/src/main/java/com/etherealdev/financemanager/MainActivity.kt
+++ app/src/main/java/com/etherealdev/financemanager/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.etherealdev.financemanager
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,10 +11,20 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import com.etherealdev.financemanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var button: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_layout)
 
+        button = findViewById(R.id.button)
 
+        button.text = getString(R.string.button_text)
+        button.setTextColor(Color.GREEN)
+        button.setBackgroundColor(Color.BLACK)
+    }
 }
